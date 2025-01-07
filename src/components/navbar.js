2dc963b7859fbc6b243226a4d39fbca3b845b7a2
cristito <cdpsk8@gmail.com>
2025-01-07 13:25:49 -0500

    nav final

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
       ),
       React.createElement('div', { className: 'social-links' },
         React.createElement('a', { href: 'https://www.instagram.com/hanapaints_/', target: '_blank', rel: 'noopener noreferrer' },
-          React.createElement('img', { src: '/fb.png', alt: 'Facebook' })
+          React.createElement('img', { src: '/fb.png', alt: 'instagram' })
         ),
         React.createElement('a', { href: 'https://www.instagram.com/hanapaints_/', target: '_blank', rel: 'noopener noreferrer' },
           React.createElement('img', { src: '/ig.png', alt: 'instagram' })
